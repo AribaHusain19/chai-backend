93072de5eeebb47a33b3a62934a617b97e6b5152
Ariba Husain <aribahusain19082004@gmail.com>
2024-08-26 12:27:16 +0530

    multer config

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import {v2 as cloudinary} from "cloudinary"
+import { response } from "express";
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+
+const uploadOnCloudinary=async (localFilePath) =>{
+    try {
+        if(!localFilePath)
+        {
+            return null
+        }
+        //upload the file on cloudinary
+        const response=await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+        //file uploaded successfully
+        console.log("file uploaded successfully on cloudinary",
+            response.url);
+            return response;
+         
+    } catch (error) {
+        fs.unlinkSync(localFilePath)//remove the locally saved temporary files as the upload operation failed;
+        return null;
+        
+    }
+}
+
+export {uploadOnCloudinary}
